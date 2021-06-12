db417b2837aba43cf3ceaaab28ae373b780a1ad2
Mike Delmonaco <mthundershock@gmail.com>
2021-06-12 12:22:02 -0400

    builder combinators, more features, some testing

--- src/JMESPath.hs
+++ src/JMESPath.hs
@@ -1,61 +1,79 @@
+{-# LANGUAGE FlexibleInstances #-}
 module JMESPath
     ( 
     -- * Core Types
       Selector()
     , Expr()
+    , IsExpr(..)
     -- * Evaluation
     , eval
     , (.?)
-    -- * Builder Combinators
-    -- ** Expr Combinators
+    -- * Expr Combinators
     , (|>)
-    , (^|>)
-    -- ** Selector Combinators
+    -- * Selector Combinators
     , (?.)
     , (?..)
+    , (?.*)
     , (?!)
-    , (?:)
+    , (?!:)
     , Slice(..)
+    , (?!*)
+    , (?&)
+    , prop
+    , multiSelect
+    , index
+    , slice
     , arrayWild
     , objWild
+    , remap
     , select
     ) where
 
 import Data.Text (Text)
 import Data.Aeson ( Array, Object, Value(Object, Null, Array) )
 import qualified Data.HashMap.Strict as Map
-import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Maybe (fromMaybe)
 import Data.Function ((&))
 import qualified Data.Vector as Vector
 import Data.Vector(Vector)
 import Data.Foldable ()
 import Control.Monad (join)
-import Control.Arrow ((>>>))
+import Control.Arrow ((>>>), Arrow (second))
+import Data.HashMap.Strict (HashMap)
+
+-- TODO haddoc repls
 
 -- | Represents a Python-like list slice. Slice start stop step
 data Slice = MkSlice (Maybe Int) (Maybe Int) (Maybe Int) deriving(Eq, Ord)
 
 -- TODO finish and test edge cases
-slice :: Slice -> Vector a -> Vector a
-slice (MkSlice start stop _) v = Vector.slice start' len v
+runSlice :: Slice -> Vector a -> Vector a
+runSlice (MkSlice start stop _) v = Vector.slice start' len v
     where
         start' = fromMaybe 0 start
         stop' = fromMaybe (Vector.length v) stop
         len = stop' - start'
         -- step' = fromMaybe 1 step
 
+-- TODO arbitrary selectors in multi-select
+
 -- | Represents query selectors. Field accesses, indexing, etc. Also used for projections
 data Selector
     = Prop Text
     -- ^ .x
-    | Props [Text]
-    -- ^ .[x,y,z]
+    | MultiSelect [Expr]
+    -- ^ .[x,y.z,z[0]]
+    | Remap (HashMap Text Expr)
+    -- ^ .{firstName: name.first, lastName: name.last}
     | Index Int
     -- ^ [0]
     | Slice Slice
     -- ^ [3:5]
     | ObjectProjection
     -- ^ .*
+    -- TODO flatten
+    -- TODO function
+    -- TODO filter
     deriving(Eq, Ord)
 
 -- | Helper for matching on an object
@@ -90,15 +108,23 @@ selects (selector:selectors) v =
         goProj = Vector.toList >>> fmap go >>> Vector.fromList >>> Array
     in case selector of
         Prop name -> go $ fromMaybe Null $ v `onObject'` \object -> object Map.!? name
-        Props names -> go $ fromMaybe Null $ v `onObject` \object ->
-            mapMaybe (`Map.lookup` object) names
+        MultiSelect sss -> go $
+            eval v <$> sss
+            & filter (/= Null)
             & Vector.fromList
             & Array
+        Remap mapping -> go $
+            mapping
+            & Map.toList
+            & fmap (second (eval v))
+            & Map.fromList
+            & Object
         Index i -> go $ fromMaybe Null $ v `onArray'` \vector -> vector Vector.!? i
-        Slice s -> goProj $ fromMaybe mempty $ v `onArray` slice s
+        Slice s -> goProj $ fromMaybe mempty $ v `onArray` runSlice s
         ObjectProjection -> goProj $ fromMaybe mempty $ v `onObject` \object -> foldMap Vector.singleton object
 
 -- Maybe you could make lenses/traversals for these! could use function composition with (.)!!
+-- Selector composition isn't context-free, but Exprs could certainly be a lens/traversal
 
 -- | Represents a full JMESPath expression/query. Use `Monoid` instance or combinators for pipes
 data Expr
@@ -116,56 +142,102 @@ instance Semigroup Expr where
 instance Monoid Expr where
     mempty = Selects mempty
 
+class IsExpr e where
+    toExpr :: e -> Expr
+
+instance IsExpr Expr where
+    toExpr = id
+
+instance IsExpr [Selector] where
+    toExpr = Selects
+
+instance IsExpr (Vector Selector) where
+    toExpr = toExpr . Vector.toList
+
+instance IsExpr Selector where
+    toExpr s = toExpr [s]
+
+
 -- | Evaluate an expression on a JSON value
+infixl 4 `eval`
 eval :: Value -> Expr -> Value
 eval v = \case
     Selects selectors -> selects selectors v
-    Pipe l r -> v .? l .? r
+    Pipe l r -> v `eval` l `eval` r
 
--- | infix alias for `eval`
+-- | Runs the selectors on the value
 infixl 4 .?
-(.?) :: Value -> Expr -> Value
-(.?) = eval
+(.?) :: IsExpr e => Value -> e -> Value
+(.?) v = eval v . toExpr
 
--- combinators for building `Expr`s
-
-infixl 5 |>, ^|>
--- | Pipes result of left expression into right selectors
-(|>) :: Expr -> [Selector] -> Expr
-e |> ss = e <> Selects ss
-
--- | Pipes result of left selectors into right selectors
-(^|>) :: [Selector] -> [Selector] -> Expr
-ss ^|> ss' = Selects ss <> Selects ss'
+infixl 5 |>
+-- | Pipes result of left expression into right selectors. Alias for `(.?)`
+(|>) :: (IsExpr e, IsExpr e') => e -> e' -> Expr
+l |> r = Pipe (toExpr l) (toExpr r)
 
 -- combinators for building `Selector` lists
 
-infixl 6 ?., ?.., ?!, ?:
+infixl 6 ?., ?.., ?!, ?!:, ?&, ?.*
 
--- | Field accessor
+-- | Field selector
 (?.) :: [Selector] -> Text -> [Selector]
 s ?. f = s <> [Prop f]
 
--- | Multi-field accessor
-(?..) :: [Selector] -> [Text] -> [Selector]
-s ?.. fs = s <> [Props fs]
+-- | Multi selector (does not project)
+(?..) :: IsExpr e => [Selector] -> [e] -> [Selector]
+s ?.. es = s <> [MultiSelect (toExpr <$> es)]
 
--- | Index accessor
+-- | Object projection
+(?.*) :: [Selector] -> Selector -> [Selector]
+ss ?.* s = ss ?& objWild ?& s
+
+-- | Index selector
 (?!) :: [Selector] -> Int -> [Selector]
 s ?! i = s <> [Index i]
 
--- | Slice accessor
-(?:) :: [Selector] -> Slice -> [Selector]
-s ?: sl = s <> [Slice sl]
+-- | Slice selector
+(?!:) :: [Selector] -> Slice -> [Selector]
+s ?!: sl = s <> [Slice sl]
 
--- | slices the whole array (used for list projections)
+-- | Array projection
+(?!*) :: [Selector] -> Selector -> [Selector]
+ss ?!* s = ss ?& arrayWild ?& s
+
+-- | Add a selector to the list
+(?&) :: [Selector] -> Selector -> [Selector]
+ss ?& s = ss <> [s]
+
+-- | Field selector
+prop :: Text -> Selector
+prop = Prop
+
+-- | Multi selector (does not project)
+multiSelect :: IsExpr e => [e] -> Selector
+multiSelect = MultiSelect . fmap toExpr
+
+-- | Index selector
+index :: Int -> Selector
+index = Index
+
+-- | Slice selector
+slice :: Maybe Int -> Maybe Int -> Maybe Int -> Selector
+slice start stop end = Slice (MkSlice start stop end)
+
+-- | slices the whole array (projects)
 arrayWild :: Selector
 arrayWild = Slice (MkSlice Nothing Nothing Nothing)
 
--- | slices an object's values (used for object projections)
+-- | slices an object's values (projects)
 objWild :: Selector
 objWild = ObjectProjection
 
--- | base case for selections (identity selector)
+-- | evaluates exprs on value and maps results to names to create a new object (does not project)
+remap :: IsExpr e => [(Text, e)] -> Selector
+remap =
+    fmap (second toExpr)
+    >>> Map.fromList
+    >>> Remap
+
+-- | base case for selection list (performs no transformation). Useful for combinator expressions
 select :: [Selector]
 select = mempty
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists #-}
+import Test.Hspec
+
+import Data.Aeson
+import JMESPath
+
+pass :: Expectation
+pass = True `shouldBe` True
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "prop select" $ do
+        it "works when field is present" $ do
+            Object ("a" .= True) .? prop "a" `shouldBe` Bool True
+            Object ("a" .= True <> "b" .= False) .? prop "a" `shouldBe` Bool True
+            Object ("a" .= True <> "b" .= False <> "c" .= Number 9) .? prop "a" `shouldBe` Bool True
+        it "handles absent field" $ do
+            Bool True .? prop "a" `shouldBe` Null
+            Bool False .? prop "a" `shouldBe` Null
+            Null .? prop "a" `shouldBe` Null
+            Number 3 .? prop "a" `shouldBe` Null
+            String "a" .? prop "a" `shouldBe` Null
+            Array [] .? prop "a" `shouldBe` Null
+            Array [Object ("a" .= Bool True)] .? prop "a" `shouldBe` Null
+        it "chains" $ do
+            Object ("a" .= Object ("b" .= Object ("c" .= Bool True))) .? select ?. "a" ?. "b" ?. "c" `shouldBe` Bool True
+    describe "index select" $ do
+        it "works when item is present" $ do
+            Array [Bool True] .? index 0 `shouldBe` Bool True
+            Array [Bool True, Bool False, Number 9] .? index 0 `shouldBe` Bool True
+            Array [Bool True, Bool False, Number 9] .? index 1 `shouldBe` Bool False
+            Array [Bool True, Bool False, Number 9] .? index 2 `shouldBe` Number 9
+        it "works when item is not present " $ do
+            Array [] .? index 0 `shouldBe` Null
+            Object mempty .? index 0 `shouldBe` Null
+            Bool True .? index 0 `shouldBe` Null
+            Bool False .? index 0 `shouldBe` Null
+            Number 0 .? index 0 `shouldBe` Null
+            String "a" .? index 0 `shouldBe` Null
+            Null .? index 0 `shouldBe` Null
+            Array [Bool True] .? index (-1) `shouldBe` Null
+            Array [Bool True] .? index 1 `shouldBe` Null
+        it "chains" $ do
+            Array [Array [Array [Bool True]]] .? select ?! 0 ?! 0 ?! 0 `shouldBe` Bool True
+    describe "multi select" $ do
+        it "works when all succeed" $ do
+            Object ("a" .= Number 1 <> "b" .= Number 2 <> "c" .= Number 3) .? multiSelect [prop "a", prop "b"] `shouldBe` Array [Number 1, Number 2]
+        it "works when none succeed" $ do
+            Object mempty .? multiSelect [prop "a", prop "b"] `shouldBe` Array []
+        it "works when some succeed" $ do
+            Object ("a" .= Number 1 <> "b" .= Number 2) .? multiSelect [prop "a", prop "c", prop "b"] `shouldBe` Array [Number 1, Number 2]
+        it "doesn't project" $ do
+            Object ("a" .= Array [Number 1] <> "b" .= Array [Number 2]) .? select ?& multiSelect [prop "a", prop "b"] ?! 0 `shouldBe` Array [Number 1]
